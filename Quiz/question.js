b7a6e43813d468b020fcb403c2094fbfb4b68022
azfarsuhail <138823481+azfarsuhail@users.noreply.github.com>
2023-10-22 01:16:25 +0500

    Add files via upload

---
+++ Quiz/question.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const quizQuestions = [
+    {
+        question: 'What is the capital of France?',
+        choices: ['Paris', 'Berlin', 'London', 'Madrid'],
+        correctAnswer: 'Paris',
+    },
+    {
+        question: 'Which planet is known as the Red Planet?',
+        choices: ['Mars', 'Venus', 'Jupiter', 'Saturn'],
+        correctAnswer: 'Mars',
+    },
+    // Add more questions here
+];
+exports.default = quizQuestions;
---
+++ Quiz/question.ts
@@ -0,0 +1,16 @@
+const quizQuestions = [
+    {
+      question: 'What is the capital of France?',
+      choices: ['Paris', 'Berlin', 'London', 'Madrid'],
+      correctAnswer: 'Paris',
+    },
+    {
+      question: 'Which planet is known as the Red Planet?',
+      choices: ['Mars', 'Venus', 'Jupiter', 'Saturn'],
+      correctAnswer: 'Mars',
+    },
+    // Add more questions here
+  ];
+  
+  export default quizQuestions;
+  
