cf34273321a48ed9c3363b2a88e47eb584805588
azfarsuhail <138823481+azfarsuhail@users.noreply.github.com>
2023-10-22 01:07:06 +0500

    Add files via upload

---
+++ Countdown Timer/index.js
@@ -0,0 +1,21 @@
+import { format, differenceInSeconds } from 'date-fns';
+function getRemainingTime(targetDate) {
+    const currentDate = new Date();
+    return differenceInSeconds(targetDate, currentDate);
+}
+function startCountdown(targetDate) {
+    const interval = setInterval(() => {
+        const remainingTime = getRemainingTime(targetDate);
+        if (remainingTime <= 0) {
+            console.log('Countdown expired!');
+            clearInterval(interval);
+        }
+        else {
+            const formattedTime = format(new Date(remainingTime * 1000), 'HH:mm:ss');
+            console.log(`Time remaining: ${formattedTime}`);
+        }
+    }, 1000);
+}
+const targetDate = new Date('2023-12-31T23:59:59'); // Set your target date and time here
+console.log(`Countdown to: ${format(targetDate, 'MMMM d, yyyy HH:mm:ss')}`);
+startCountdown(targetDate);
